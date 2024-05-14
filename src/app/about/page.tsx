bd850ac95ecfd1ff5c4d2b2cadb5a577eff29491
Vitor George <vitor.george@gmail.com>
2024-05-14 10:57:21 +0100

    Scaffold about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,69 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor
+        consectetur risus, id vehicula libero aliquam non. Phasellus et nisl a
+        eros aliquam imperdiet a eu odio. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia curae; Vivamus a tellus
+        scelerisque, tincidunt magna commodo, faucibus ipsum. Aliquam erat
+        volutpat. Phasellus pellentesque mauris et tristique vehicula. Sed
+        ullamcorper efficitur velit, sit amet scelerisque mauris pellentesque
+        eu. Donec mattis, est eu consectetur cursus, lectus dui porta lacus, sit
+        amet accumsan nulla diam vel enim. Nulla lobortis, urna ac suscipit
+        imperdiet, lectus est congue ante, in egestas ex ex sed magna. Integer
+        finibus efficitur urna vel porta. Aenean ultrices, libero a consequat
+        interdum, mi lorem efficitur turpis, laoreet mattis sapien elit quis
+        nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+        posuere cubilia curae;
+      </p>
+      <p>
+        Morbi elit dolor, accumsan eu enim in, pretium sagittis purus. Donec at
+        odio mattis, elementum est aliquam, facilisis felis. Etiam dolor quam,
+        tempus vitae blandit volutpat, facilisis eu urna. Cras ultricies ipsum
+        non leo feugiat ultricies eu non justo. Morbi feugiat libero nec viverra
+        condimentum. Ut at lectus ac est vestibulum tincidunt. Fusce a enim
+        ipsum. Maecenas scelerisque lacus in feugiat ultrices. Duis dictum, est
+        eu suscipit iaculis, urna purus cursus magna, nec fermentum nunc leo a
+        eros. Nam felis dolor, pretium vel sem id, pulvinar ultricies elit.
+        Aenean imperdiet hendrerit leo in blandit.
+      </p>
+      <p>
+        Vivamus mi tellus, ullamcorper a semper sed, finibus id sem. Duis
+        posuere sit amet lorem a efficitur. Aenean aliquam velit a tristique
+        pretium. Duis justo turpis, venenatis vel ligula nec, porttitor feugiat
+        tellus. Nam lobortis sodales dictum. Proin pellentesque varius tellus eu
+        hendrerit. Duis cursus pulvinar nunc, ut ullamcorper urna molestie non.
+        Donec sagittis feugiat faucibus. Donec interdum vulputate lorem vitae
+        eleifend. Suspendisse iaculis aliquet est a gravida. Nullam consectetur
+        justo eget justo suscipit, tincidunt varius odio sagittis. Aliquam eu
+        nisl sit amet est efficitur consectetur. Integer posuere odio sed
+        fermentum ornare. Ut dictum ornare eros, a semper nunc suscipit feugiat.
+        Proin euismod efficitur felis a egestas.
+      </p>
+      <p>
+        Nulla malesuada risus non eros scelerisque, et porta eros tincidunt.
+        Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras at
+        blandit libero, at imperdiet velit. Phasellus maximus a risus non
+        dignissim. Praesent ut turpis magna. Etiam nec arcu ac turpis ultrices
+        luctus ut tempor leo. Nam metus justo, pretium ac lacus quis, posuere
+        efficitur massa. Aenean placerat luctus ante, quis semper erat semper
+        at. Duis id urna lorem. In tempor dui at odio pretium sagittis. Aenean
+        eget erat dui. Phasellus aliquam ultrices ante, sit amet aliquam libero
+        volutpat sed. In et urna urna.
+      </p>
+      <p>
+        Donec suscipit elit ut est porta pellentesque. Vestibulum a orci lectus.
+        Nunc non vulputate arcu. Ut pretium a nibh eget lobortis. Duis erat
+        elit, vulputate non rhoncus et, bibendum at odio. Donec elementum diam
+        quis nibh fermentum, vel congue sem pharetra. Phasellus vehicula
+        elementum mi in porttitor. Duis in turpis et nisi rhoncus mattis.
+        Maecenas pretium efficitur quam ac volutpat. Pellentesque pretium lorem
+        ornare, commodo lacus consectetur, condimentum tortor. Nam dictum orci
+        aliquam lacus faucibus, vitae pulvinar nunc feugiat. Praesent ac
+        eleifend metus, in fermentum magna.
+      </p>
+    </div>
+  );
+}
