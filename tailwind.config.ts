597162465d3a77e44fc3523f171634004d1ec7df
Vitor George <vitor.george@gmail.com>
2024-05-14 12:21:44 +0100

    Scaffold globe view page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,11 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        128: "32rem",
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
