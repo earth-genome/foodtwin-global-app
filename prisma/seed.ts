a7a0c4904327f1d352871385e3a2c35fafda3d19
Vitor George <vitor.george@gmail.com>
2024-07-15 15:14:31 +0100

    Update edge schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,6 +14,8 @@ const ADMIN_LIMITS_PATH = path.join(SEED_DATA_PATH, "admin_polygons.gpkg");
 const ADMIN_LIMITS_TABLENAME = "admin_polygons";
 const NODES_MARITIME_FILE = "nodes_maritime.gpkg";
 const NODES_MARITIME_TABLENAME = "nodes_maritime";
+const EDGES_MARITIME_FILE = "edges_maritime_corrected.gpkg";
+const EDGES_MARITIME_TABLENAME = "edges_maritime_corrected";
 
 async function ingestData() {
   try {
@@ -35,6 +37,7 @@ async function ingestData() {
     // Truncate all tables
     // await prisma.$executeRaw`TRUNCATE "Area" RESTART IDENTITY CASCADE`;
     await prisma.$executeRaw`TRUNCATE "Node" RESTART IDENTITY CASCADE`;
+    await prisma.$executeRaw`TRUNCATE "Edge" RESTART IDENTITY CASCADE`;
 
     // Check if admin centroids file exists
     if (!fs.existsSync(ADMIN_CENTROIDS_PATH)) {
@@ -74,8 +77,38 @@ async function ingestData() {
     console.log("Ingesting maritime nodes...");
     await runOgr2Ogr(
       NODES_PATH,
-      `-nln Node -append -nlt POINT -lco GEOMETRY_NAME=geom -t_srs EPSG:3857 -sql "SELECT ID as id, name, upper(infra) as type, geom FROM ${NODES_MARITIME_TABLENAME}"`
+      `-nln Node -append -nlt POINT -lco GEOMETRY_NAME=geom -t_srs EPSG:3857 -sql "SELECT ID as id_str, name, upper(infra) as type, geom FROM ${NODES_MARITIME_TABLENAME}"`
     );
+
+    // Check if edges file exists
+    const EDGES_PATH = path.join(SEED_DATA_PATH, EDGES_MARITIME_FILE);
+    if (!fs.existsSync(EDGES_PATH)) {
+      console.error("Maritime edges file not found.");
+      return;
+    }
+
+    console.log("Ingesting maritime edges...");
+    await prisma.$executeRaw`DROP TABLE IF EXISTS "edge_temp"`;
+
+    await runOgr2Ogr(
+      EDGES_PATH,
+      `-nln edge_temp -append -nlt LINESTRING -lco GEOMETRY_NAME=geom -t_srs EPSG:3857 -sql "SELECT from_id as from_id_str, to_id as to_id_str, distance, length, geom FROM ${EDGES_MARITIME_TABLENAME}"`
+    );
+
+    await prisma.$executeRaw`
+      INSERT INTO "Edge" ("fromNodeId", "toNodeId", "distance", "geom")
+      SELECT
+        n1."id" AS fromNodeId,
+        n2."id" AS toNodeId,
+        e."distance",
+        e."geom"
+      FROM "edge_temp" e
+      JOIN "Node" n1 ON e."from_id_str" = n1."id_str"
+      JOIN "Node" n2 ON e."to_id_str" = n2."id_str"
+      WHERE n1."id" IS NOT NULL AND n2."id" IS NOT NULL
+    `;
+
+    await prisma.$executeRaw`DROP TABLE IF EXISTS "edge_temp"`;
   } catch (error) {
     console.error("Error ingesting data:", error);
   } finally {
