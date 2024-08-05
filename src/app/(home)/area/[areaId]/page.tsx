249f15bfe482b01721a8ef16af522b2d3b4da91a
Vitor George <vitor.george@gmail.com>
2024-08-05 17:27:11 +0100

    Load area from database

--- src/app/(home)/area/[areaId]/page.tsx
+++ src/app/(home)/area/[areaId]/page.tsx
@@ -1,13 +1,26 @@
-const AreaPage = ({
+import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
+
+const AreaPage = async ({
   params,
 }: {
   params: {
     areaId: string;
   };
 }) => {
+  const area = await prisma.area.findUnique({
+    where: {
+      id: params.areaId,
+    },
+  });
+
+  if (!area) {
+    return redirect("/not-found");
+  }
+
   return (
     <div className="overflow-y-auto w-128 p-4 space-y-4">
-      This is area page for {params.areaId}
+      This is area page for {area.name}
     </div>
   );
 };
