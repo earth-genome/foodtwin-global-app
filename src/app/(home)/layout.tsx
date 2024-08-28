df711a35cf6957c88e2cbe2f5c6c66774a2f60ae
Oliver Roick <oliver@roick.email>
2024-08-28 11:36:18 +1000

    feat: Add menu

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,10 +1,14 @@
+import Menu from "../components/Menu";
 import Globe from "./globe";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen">
-      <Globe />
-      {children}
-    </div>
+    <>
+      <Menu />
+      <div className="flex h-screen">
+        <Globe />
+        {children}
+      </div>
+    </>
   );
 }
