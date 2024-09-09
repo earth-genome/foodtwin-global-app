2c7af5605da6d3bf7945d42b4c7eef83551dfa36
Oliver Roick <oliver@roick.email>
2024-09-10 09:35:10 +1000

    fix: Set popup offset

--- src/app/components/MapPopup.tsx
+++ src/app/components/MapPopup.tsx
@@ -21,6 +21,7 @@ function MapPopup({ id, label, itemType, longitude, latitude }: IMapPopup) {
       latitude={latitude}
       maxWidth="250px"
       closeButton={false}
+      offset={12}
     >
       <div className="flex gap-2 items-center font-header tracking-tighter text-white bg-neutral-800 p-2 rounded">
         {getTypeIcon(itemType)}
