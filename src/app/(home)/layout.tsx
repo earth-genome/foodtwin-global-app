80724339c65d6a6d0bda258190bd8034cdd1d54e
Oliver Roick <oliver@roick.email>
2024-09-19 15:27:59 +1000

    feat: Add search button

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import { MagnifyingGlass } from "@phosphor-icons/react/dist/ssr";
 import Menu from "../components/menu";
 import Globe from "./globe";
 
@@ -5,6 +8,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Menu />
+      <div className="absolute right-4 top-4 z-40">
+        <Button
+          href="/search"
+          as={Link}
+          startContent={<MagnifyingGlass size={20} className="text-neutral-400" />}
+          className="rounded border-neutral-600 border bg-neutral-100/60 text-neutral-800 font-header text-xs tracking-tight"
+        >
+          Search the map
+        </Button>
+      </div>
       <div className="flex h-screen">
         <Globe />
         {children}
