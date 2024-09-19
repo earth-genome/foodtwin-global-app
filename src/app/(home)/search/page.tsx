03b165c845d91cf1474dc1f2167a035ba25af2ca
Oliver Roick <oliver@roick.email>
2024-09-19 16:44:53 +1000

    feat: Add error message

--- src/app/(home)/search/page.tsx
+++ src/app/(home)/search/page.tsx
@@ -4,35 +4,43 @@ import { useState } from "react";
 
 function SearchPage() {
   const [value, setValue] = useState<string>("");
-  const [showPlaceholder, setShowPlaceHolder] = useState<boolean>(true);
+  const [hasFocus, setHasFocus] = useState<boolean>(false);
 
-  const togglePlaceholder = () => {
-    setShowPlaceHolder((prev) => !prev);
-  };
+  const toggleFocus = () => setHasFocus((prev) => !prev);
+
+  const error =
+    value.length > 0 &&
+    value.length < 3 &&
+    "Enter at least 3 letters to start searching.";
 
   return (
     <div className="absolute top-0 left-0 z-40 h-screen w-screen bg-neutral-100/50 backdrop-blur">
       <div className="max-w-[700px] mx-auto py-20 box-border h-screen overflow-auto">
-        <div className="flex items-center gap-4 border-b pb-2 border-neutral-600">
+        <div className="flex items-center gap-4 border-b pb-2 mb-8 border-neutral-600">
           <input
             type="text"
             aria-label="Enter any region, county or port name"
+            aria-invalid={!!error}
+            aria-errormessage="search-error"
             placeholder={
-              showPlaceholder
-                ? "Enter any region, county or port name"
-                : undefined
+              hasFocus ? undefined : "Enter any region, county or port name"
             }
-            className="w-full text-3xl text-neutral-500 outline-none"
+            className={`w-full text-3xl ${hasFocus ? "text-neutral-900" : "text-neutral-500"} outline-none`}
             value={value}
             onChange={(e) => setValue(e.target.value)}
-            onFocus={togglePlaceholder}
-            onBlur={togglePlaceholder}
+            onFocus={toggleFocus}
+            onBlur={toggleFocus}
             style={{
               backgroundColor: "transparent",
             }}
           />
           <MagnifyingGlass size={20} className="text-neutral-700" />
         </div>
+        {error && (
+          <div id="search-error" className="text-2xl text-neutral-600">
+            {error}
+          </div>
+        )}
       </div>
     </div>
   );
