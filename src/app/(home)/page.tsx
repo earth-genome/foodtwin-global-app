3d4b67bf488edc2b8a67957c9136117961b3b8c5
Oliver Roick <oliver@roick.email>
2024-09-20 11:14:15 +1000

    chore: Move search button to home component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,22 @@
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import { MagnifyingGlass } from "@phosphor-icons/react/dist/ssr";
+
 const Home = () => {
-  return null;
+  return (
+    <div className="absolute right-4 top-4 z-40">
+      <Button
+        href="/search"
+        as={Link}
+        startContent={
+          <MagnifyingGlass size={20} className="text-neutral-400" />
+        }
+        className="rounded border-neutral-600 border bg-neutral-100/60 text-neutral-800 font-header text-xs tracking-tight"
+      >
+        Search the map
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
