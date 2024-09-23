503beaa7a79d074b2d924aa18013f0ca3c0784e4
Vitor George <vitor.george@gmail.com>
2024-09-23 12:22:46 +0100

    Fix diagram layout, clear unused state

--- src/app/components/map/state/machine.ts
+++ src/app/components/map/state/machine.ts
@@ -4,7 +4,7 @@ import { StateContext, StateActions, StateEvents } from "./types";
 
 export const globeViewMachine = createMachine(
   {
-    /** @xstate-layout N4IgpgJg5mDOIC5RQDYHsBGYBqBLMA7gHQCyAhgA4AEusV6ZEuAdlAMQRrNhEsBuaANY8AZmAAuAYwAW5CgBEy4sgG0ADAF1EoCmli5xuLtpAAPRAEYALAFYiAThsB2J-YAc9gGxqATF4DMTgA0IACeiAC0bhZE-jZuTv7+9u5uVm7+VgC+WSGomDj4xHI0dAxMrGxgAE7VaNVEFChKIvUAtkRiUrKUisrqWkgguvqGxkPmCMluRBY2VmrWFvZWPpkWIeEIERZunkRONn7RLnE+Pp6eOXnoWHiERACSzAa4ZChU+VhUfEVsAMpgFBgSTiKhkapgVSaEwjV7jUCTNSbRBqa4gL6FB4AYTQAFdmOJqqFSlRYECQeJIGxscCIeDIWQyRTQUZmANYXp4cwTEiUQg0bkMbcscRcQSiSTaMzgaDqYDZWCIVCOUM4WMeRNIv41ERPPZrGorPYfK5PLZTfyrBYnEQrFYktavJ4-D5skLMfdiAAFMgwUltfGEljsX3+6RkOiBiWQVU6Lka3mWWx2zwWQILNxpE2+fkRHUzfwZOIm2zufwWHJC5hoCBwEyeoqc0ZspPbJwzfWG42mry2fx59IzNbWLNqOL2xLoxsPErS8oh5vcttOKx6qwu+xnCxqRIrPM+CwxNzxFw2LwWQ+Vj0ir1PF6Gd6fEU-JtqhOtrUIOb+IjRfz6moQEdm6-KOH+q6eB2HhFrs0TTreRREOKhLEqS5KKpAS6Jl+FjeAc45xJcZ5dvySQxFu5xOD4ajeOOThXDeBR3gAqtwpgUJSkBUDUdTVNhn6IogdGzCsiw2EciwrAOYSRHBRA2LslHxMaUE2IxNzMUhYZgAGQaGKwAkImYlhHEQtELDqW7uE44mDos5n2jRajOPMyzujkQA */
+    /** @xstate-layout N4IgpgJg5mDOIC5RQDYHsBGYBqBLMA7gHS4B2uALrgIYoDEYAbmKRQtQE5jUKxgpgAxhQDaABgC6iUAAc0sSrjSlpIAB6IAjAE4AbETFiALACYjADnOmA7GO2aAzABoQAT0QBaTZv1jzusU0AVmCHTXMg62sAX2iXVEwcfGJObl5+IQpIBmZWdi4eQQFOcSkkEDkFKmVVDQQPByJrcOswyN0TB21m7Rd3eu9ff0CQoLCI611Y+PQsPEIiVJ4+AWFsphY2JfTV0UlVSsUa8rrNE2siHyNNMSCg3V0HMWbrPq0HIKJza3NHbzHzH5NNMQAk5skiDJqDAEABbNAAV1YZCgOU2CChMPhSKyEFKB3kRxUJ08JjERAczXMlPuZiCt10QTe9QcHQMH10Okeula1jMsTiIFIaAgcFUYKShAJVSUxNAdQ8unMFKpNI6RnpkWZHjGRiIRl02j813M2iCgIcIIl82IZEUtGlRNqiEpBnMJiC3Q9ZgclO1Jk0ep+Su8H2e1w+VtmkpSBR2mUgjuqcvUiAuYUpmkmRlMfjJrzcWnJrW09kCWZzjMcUcSNsh0LAcMRyNIUCTsudCE0XzC10DhuMXSMem1OYuJl+gPp4TD5wF0SAA */
     id: "globeView",
 
     types: {
@@ -41,8 +41,6 @@ export const globeViewMachine = createMachine(
           },
         },
       },
-
-      "Unexpected error": {},
 
       "page:mounting": {
         on: {
