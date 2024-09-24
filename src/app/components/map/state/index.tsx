51ef516d013e2e9c693a283c823e09233f7c2271
Vitor George <vitor.george@gmail.com>
2024-09-24 10:56:15 +0100

    Remove stately inspector in favor of a custom logger

--- src/app/components/map/state/index.tsx
+++ src/app/components/map/state/index.tsx
@@ -1,15 +1,39 @@
 import { createActorContext } from "@xstate/react";
 import { globeViewMachine } from "./machine";
-import { createBrowserInspector } from "@statelyai/inspect";
 
-export const MachineContext = createActorContext(
-  globeViewMachine,
-  process.env.NEXT_PUBLIC_USE_STATELY_INSPECTOR === "true"
-    ? {
-        inspect: createBrowserInspector().inspect,
-      }
-    : undefined
-);
+export const MachineContext = createActorContext(globeViewMachine, {
+  inspect:
+    process.env.NEXT_PUBLIC_XSTATE_INSPECT === "true"
+      ? (inspectEvent) => {
+          if (
+            inspectEvent.type !== "@xstate.event" &&
+            inspectEvent.type !== "@xstate.snapshot"
+          )
+            return;
+
+          console.groupCollapsed(
+            "%c event",
+            "color: gray; font-weight: lighter;",
+            inspectEvent.type
+          );
+
+          if (inspectEvent.type === "@xstate.event") {
+            console.log(
+              "%c event",
+              "color: #03A9F4; font-weight: bold;",
+              inspectEvent.event
+            );
+          } else if (inspectEvent.type === "@xstate.snapshot") {
+            console.log(
+              "%c state",
+              "color: #4CAF50; font-weight: bold;",
+              inspectEvent.snapshot
+            );
+          }
+          console.groupEnd();
+        }
+      : undefined,
+});
 
 export function MachineProvider(props: { children: React.ReactNode }) {
   return <MachineContext.Provider>{props.children}</MachineContext.Provider>;
