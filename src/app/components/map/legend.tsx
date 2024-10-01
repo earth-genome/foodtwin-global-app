e8a35ae2828f6a923d2b83de56e91c16f9b6b82f
Oliver Roick <oliver@roick.email>
2024-10-01 16:52:50 +1000

    feat: Add expand/collapse to legend

--- src/app/components/map/legend.tsx
+++ src/app/components/map/legend.tsx
@@ -1,3 +1,6 @@
+import { ArrowDown, ArrowUp } from "@phosphor-icons/react";
+import { useState } from "react";
+
 interface ILegendItem {
   color: string;
   label: string;
@@ -13,13 +16,26 @@ function LegendItem({ color, label }: ILegendItem) {
 }
 
 function Legend() {
+  const [isExpanded, setIsExpanded] = useState<boolean>(true);
+
   return (
-    <div className="absolute bottom-4 left-[50%] translate-x-[-50%] z-50 bg-neutral-100/60 rounded text-xs backdrop-blur">
-      <div className="px-4 py-2 bg-neutral-200/80 font-header text-xxs uppercase rounded-t">
-        <h2>Legend</h2>
-      </div>
-      <div className="flex text-neutral-600 items-center">
-        <div className="p-4 border-r border-neutral-300">
+    <div className="absolute bottom-4 left-[50%] translate-x-[-50%] z-50 bg-neutral-100/60 rounded text-xs backdrop-blur w-[530px]">
+      <button
+        className={`flex gap-4 items-center px-4 py-2 w-[100%] bg-neutral-200/80 font-header text-xxs uppercase ${isExpanded ? "rounded-t" : "rounded"}`}
+        onClick={() => setIsExpanded((prev) => !prev)}
+        aria-controls="legend-panel"
+        aria-expanded={isExpanded}
+      >
+        <span className="flex-grow text-left">Legend</span>
+        <span aria-label={`${isExpanded ? "Collapse" : "Expand"}`}>
+          {isExpanded ? <ArrowDown size={16} /> : <ArrowUp size={16} />}
+        </span>
+      </button>
+      <div
+        id="legend-panel"
+        className={`flex text-neutral-600 items-center ${isExpanded ? "block" : "hidden"}`}
+      >
+        <div className="p-4 border-r border-neutral-300 flex-grow">
           <h3 className="mb-2 font-bold">Categories</h3>
           <div className="grid grid-cols-2 gap-x-8 gap-y-2">
             <LegendItem color="neutral-900" label="Dairy and Eggs" />
@@ -34,7 +50,7 @@ function Legend() {
             <LegendItem color="neutral-900" label="Others" />
           </div>
         </div>
-        <div className="p-4">
+        <div className="p-4 flex-grow">
           <h3 className="mb-2 font-bold">Calories production</h3>
           <div className="flex gap-4 items-center">
             <span>Low</span>
