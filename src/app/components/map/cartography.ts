326167097805d3b62eec598d82af4a6129115987
Oliver Roick <oliver@roick.email>
2024-10-01 12:22:24 +1000

    refactor: Move area-layer styles to cartography module

--- src/app/components/map/cartography.ts
+++ src/app/components/map/cartography.ts
@@ -1,3 +1,24 @@
+// Colors
+const AREA_HIGHLIGHT_COLOR = "rgba(250, 250, 249, 0.7)";
+const AREA_DEFAULT_COLOR = "rgba(250, 250, 249, 0.3)";
+const AREA_HIGHLIGHT_OUTLINE_COLOR = "rgba(0, 0, 0, 1)";
+const AREA_DEFAULT_OUTLINE_COLOR = "rgba(0, 0, 0, 0.3)";
+
+export const areaStyle = {
+  "fill-color": [
+    "case",
+    ["boolean", ["feature-state", "hover"], false],
+    AREA_HIGHLIGHT_COLOR,
+    AREA_DEFAULT_COLOR,
+  ],
+  "fill-outline-color": [
+    "case",
+    ["boolean", ["feature-state", "hover"], false],
+    AREA_HIGHLIGHT_OUTLINE_COLOR,
+    AREA_DEFAULT_OUTLINE_COLOR,
+  ],
+};
+
 export const foodgroupsStyle = {
   "circle-emissive-strength": 1,
   "circle-color": [
