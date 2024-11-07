2f337207d110a244143357f67c9c9ffd2ac7bb25
Vitor George <vitor.george@gmail.com>
2024-11-07 14:42:54 +0000

    Add PlausibleProvider to root layout, delete unused file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { NextUIProvider } from "@nextui-org/react";
+import PlausibleProvider from "next-plausible";
 import { Lexend_Exa, Merriweather } from "next/font/google";
 
 const lexend = Lexend_Exa({
@@ -22,6 +23,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       lang="en"
       className={`${lexend.variable} ${merriweather.variable} font-body`}
     >
+      <head>
+        <PlausibleProvider
+          domain={process.env.NEXT_PUBLIC_PLAUSIBLE_URL || ""}
+        />
+      </head>
       <body>
         <NextUIProvider>{children}</NextUIProvider>
       </body>
