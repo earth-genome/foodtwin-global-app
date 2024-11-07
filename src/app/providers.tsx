aa42997abd85b651c87b0f206ebb8211a3dcfe02
Oliver Roick <oliver@roick.email>
2024-11-07 17:31:47 +1100

    feat: Add Plausible script

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,10 @@
 import { NextUIProvider } from "@nextui-org/react";
+import PlausibleProvider from "next-plausible";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <PlausibleProvider domain={process.env.NEXT_PUBLIC_PLAUSIBLE_URL || ""}>
+      <NextUIProvider>{children}</NextUIProvider>
+    </PlausibleProvider>
+  );
 }
