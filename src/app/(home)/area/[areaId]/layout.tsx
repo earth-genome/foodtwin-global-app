cee860630d4edc22fb6974a7d95b23dcbdddaa5c
Vitor George <vitor.george@gmail.com>
2025-02-24 13:31:36 +0000

    Remove className property from layout component

--- src/app/(home)/area/[areaId]/layout.tsx
+++ src/app/(home)/area/[areaId]/layout.tsx
@@ -1,15 +1,11 @@
-export default function Layout({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div
-      className={`w-[480px] bg-white h-screen grid grid-rows-[max-content_1fr] ${className}`}
+      className={`w-[480px] bg-white h-screen grid grid-rows-[max-content_1fr] overflow-hidden`}
     >
       {children}
     </div>
   );
-}
+};
+
+export default Layout;
