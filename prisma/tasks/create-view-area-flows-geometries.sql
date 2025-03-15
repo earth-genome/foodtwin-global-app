47c7e2fd8bbccadbd04c6240ce1768f945fd1e02
Vitor George <vitor.george@gmail.com>
2025-03-15 20:27:40 +0000

    fix: generate one geometry per OD, move food group flows per area to separate view

--- prisma/tasks/create-view-area-flows-geometries.sql
+++ prisma/tasks/create-view-area-flows-geometries.sql
@@ -1,74 +1,52 @@
-DROP MATERIALIZED VIEW IF EXISTS "AreaFlowsGeometries";
+SET
+  max_parallel_workers_per_gather = 8;
 
-CREATE MATERIALIZED VIEW "AreaFlowsGeometries" AS
-WITH
-  "aggregatedFlows" AS (
-    SELECT
-      MIN("id") as "id",
-      COUNT("id") as "count",
-      "foodGroupId",
-      "fromAreaId",
-      "toAreaId",
-      "type",
-      SUM("value") as "sumValue"
-    FROM
-      "Flow"
-    WHERE
-      "Flow"."fromAreaId" = 'ESP.8_1'
-    GROUP BY
-      "foodGroupId",
-      "fromAreaId",
-      "toAreaId",
-      "type"
-  ),
-  "orderedEdges" AS (
-    SELECT
-      "Flow".id as "flowId",
-      "FlowSegment"."order" as "flowSegmentOrder",
-      "FlowSegmentEdges"."order" as "flowSegmentEdgeOrder",
-      "Edge"."geom" AS "geom"
-    FROM
-      "Flow"
-      JOIN "FlowSegment" ON "Flow"."id" = "FlowSegment"."flowId"
-      JOIN "FlowSegmentEdges" ON "FlowSegment"."id" = "FlowSegmentEdges"."flowSegmentId"
-      JOIN "Edge" ON "FlowSegmentEdges"."edgeId" = "Edge"."id"
-    WHERE
-      "Flow"."id" IN (
-        SELECT
-          "aggregatedFlows"."id"
-        FROM
-          "aggregatedFlows"
-      )
-    GROUP BY
-      "Flow".id,
-      "Edge"."geom",
-      "FlowSegment"."order",
-      "FlowSegmentEdges"."order"
-    ORDER BY
-      "Flow".id ASC,
-      "FlowSegment"."order" ASC,
-      "FlowSegmentEdges"."order" ASC
-  )
+DROP MATERIALIZED VIEW IF EXISTS "AreaFlowGeometries";
+
+CREATE MATERIALIZED VIEW "AreaFlowGeometries" AS
 SELECT
-  "aggregatedFlows"."id" AS "flowId",
-  "aggregatedFlows"."foodGroupId",
-  "aggregatedFlows"."fromAreaId",
-  "aggregatedFlows"."toAreaId",
-  "aggregatedFlows"."type",
-  "aggregatedFlows"."sumValue",
-  "aggregatedFlows"."count",
-  ST_SimplifyPreserveTopology (
-    ST_LineMerge (ST_Union ("orderedEdges"."geom")),
-    1000
-  ) AS "multiLineStringGeom"
+  "Flow".id as "flowId",
+  "Flow"."fromAreaId",
+  "Flow"."toAreaId",
+  ST_SimplifyPreserveTopology (ST_LineMerge (ST_Union (("Edge"."geom"))), 1000) as "geom"
 FROM
-  "orderedEdges"
-  JOIN "aggregatedFlows" ON "orderedEdges"."flowId" = "aggregatedFlows"."id"
+  "Flow"
+  JOIN "FlowSegment" ON "Flow"."id" = "FlowSegment"."flowId"
+  JOIN "FlowSegmentEdges" ON "FlowSegment"."id" = "FlowSegmentEdges"."flowSegmentId"
+  JOIN "Edge" ON "FlowSegmentEdges"."edgeId" = "Edge"."id"
+WHERE
+  "Flow"."id" in (
+    SELECT
+      ANY_VALUE ("id") as "id"
+    FROM
+      "Flow"
+    GROUP BY
+      "fromAreaId",
+      "toAreaId"
+  )
 GROUP BY
-  "aggregatedFlows"."id",
-  "aggregatedFlows"."foodGroupId",
-  "aggregatedFlows"."fromAreaId",
-  "aggregatedFlows"."toAreaId",
-  "aggregatedFlows"."type",
-  "aggregatedFlows"."sumValue",
-  "aggregatedFlows"."count";
+  "Flow".id,
+  "Flow"."fromAreaId",
+  "Flow"."toAreaId"
+ORDER BY
+  "Flow".id ASC,
+  "Flow"."fromAreaId",
+  "Flow"."toAreaId";
+
+DROP MATERIALIZED VIEW IF EXISTS "AreaFlowsPerFoodGroup";
+
+CREATE MATERIALIZED VIEW "AreaFlowsPerFoodGroup" AS
+SELECT
+  "Flow"."fromAreaId",
+  "Flow"."toAreaId",
+  "Flow"."foodGroupId",
+  SUM("Flow"."value") as "value"
+FROM
+  "Flow"
+GROUP BY
+  "Flow"."fromAreaId",
+  "Flow"."toAreaId",
+  "Flow"."foodGroupId"
+ORDER BY
+  "Flow"."fromAreaId",
+  "Flow"."toAreaId";
