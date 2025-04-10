b62e79a2954ade5a7522c0f6b345717b14165518
Vitor George <vitor.george@gmail.com>
2025-04-10 19:16:45 +0100

    fix: make FlowGeometry a multilinestring

--- prisma/migrations/20250410105215_add_flow_geometry/migration.sql
+++ prisma/migrations/20250410105215_add_flow_geometry/migration.sql
@@ -1,19 +1,19 @@
 -- CreateTable
-CREATE TABLE "FlowGeometry" (
-    "id" BIGSERIAL NOT NULL,
-    "fromAreaId" TEXT NOT NULL,
-    "toAreaId" TEXT NOT NULL,
-    "geometry" geometry(LineString, 4326) NOT NULL,
-    "areaId" TEXT,
-
-    CONSTRAINT "FlowGeometry_pkey" PRIMARY KEY ("id")
-);
+CREATE TABLE
+    "FlowGeometry" (
+        "id" BIGSERIAL NOT NULL,
+        "fromAreaId" TEXT NOT NULL,
+        "toAreaId" TEXT NOT NULL,
+        "geometry" geometry (MultiLineString, 4326) NOT NULL,
+        CONSTRAINT "FlowGeometry_pkey" PRIMARY KEY ("id"),
+        CONSTRAINT "unique_flow_pair" UNIQUE ("fromAreaId", "toAreaId")
+    );
 
 -- CreateIndex
-CREATE INDEX "flow_geometry_pair_idx" ON "FlowGeometry"("fromAreaId", "toAreaId");
+CREATE INDEX "flow_geometry_pair_idx" ON "FlowGeometry" ("fromAreaId", "toAreaId");
 
 -- AddForeignKey
-ALTER TABLE "FlowGeometry" ADD CONSTRAINT "FlowGeometry_fromAreaId_fkey" FOREIGN KEY ("fromAreaId") REFERENCES "Area"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "FlowGeometry" ADD CONSTRAINT "FlowGeometry_fromAreaId_fkey" FOREIGN KEY ("fromAreaId") REFERENCES "Area" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "FlowGeometry" ADD CONSTRAINT "FlowGeometry_toAreaId_fkey" FOREIGN KEY ("toAreaId") REFERENCES "Area"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "FlowGeometry" ADD CONSTRAINT "FlowGeometry_toAreaId_fkey" FOREIGN KEY ("toAreaId") REFERENCES "Area" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
