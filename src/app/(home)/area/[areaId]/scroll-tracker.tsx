9ae38d0c7f2d7b2c124da8c710a72163b991fda9
Daniel da Silva <danielfdsilva@gmail.com>
2025-05-22 15:42:08 +0100

    Ensures sidebar scrollability
    Makes sure the last section of the sidebar always has the
    same height as the sidebar itself. This ensures proper
    scrollability when the content is shorter than the sidebar.
    
    Fix #125

--- src/app/(home)/area/[areaId]/scroll-tracker.tsx
+++ src/app/(home)/area/[areaId]/scroll-tracker.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { useRouter, usePathname } from "next/navigation";
 
 interface IScrollTracker {
@@ -13,6 +13,33 @@ function ScrollTracker({ children }: IScrollTracker) {
   const [ref, setRef] = useState<HTMLDivElement>();
   const onRef = (e: HTMLDivElement) => setRef(e);
   const currentSection = useRef<string | null>("");
+
+  // Ensure that the last section of the sidebar is always the same height as
+  // the sidebar to allow for scrolling.
+  useLayoutEffect(() => {
+    if (!ref) return;
+    // Set the height of the last section to be the same as the sidebar.
+    const lastSection = ref.children[ref.children.length - 1] as HTMLDivElement;
+    const lastSectionOriginalHeight = lastSection.clientHeight;
+
+    const sidebarHeight = ref.clientHeight;
+    if (sidebarHeight > lastSectionOriginalHeight) {
+      lastSection.style.height = `${sidebarHeight}px`;
+    }
+
+    // Observe the sidebar for changes in size.
+    const resizeObserver = new ResizeObserver((e) => {
+      const sidebarHeight = e[0].contentRect.height;
+      if (sidebarHeight > lastSectionOriginalHeight) {
+        lastSection.style.height = `${sidebarHeight}px`;
+      }
+    });
+    resizeObserver.observe(ref);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [ref]);
 
   useEffect(() => {
     if (!ref) return;
