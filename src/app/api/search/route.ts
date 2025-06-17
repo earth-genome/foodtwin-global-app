37a1191a22557a1d15b5a3cfacbeabd229d918ad
Vitor George <vitor.george@gmail.com>
2025-06-17 10:38:34 +0100

    fix: do not return ports on search

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -9,7 +9,7 @@ export async function GET(req: NextRequest) {
     return Response.json({ error: "Provide a query term." }, { status: 400 });
   }
 
-  const [areas, nodes] = await Promise.all([
+  const [areas] = await Promise.all([
     prisma.area.findMany({
       where: {
         name: {
@@ -19,23 +19,10 @@ export async function GET(req: NextRequest) {
       },
       take: 10,
     }),
-    prisma.node.findMany({
-      where: {
-        name: {
-          contains: query,
-          mode: "insensitive",
-        },
-        type: {
-          equals: "PORT",
-        },
-      },
-      take: 10,
-    }),
   ]);
 
   const results = [
     ...areas.map((area) => ({ ...area, type: EItemType["area"] })),
-    ...nodes.map((node) => ({ ...node, type: EItemType["node"] })),
   ].sort((a, b) => ((a.name || "") < (b.name || "") ? -1 : 1)); // a.name || "" exists because the name can be null
   return Response.json(results);
 }
